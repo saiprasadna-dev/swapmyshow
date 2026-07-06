735c015b1f266e35c6f79c06d70a81796fc34357
MSI\saipr <narendravarapuprasad@gmail.com>
2026-07-06 19:50:17 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Hello Cloudflare 🚀</h1>
+
+      <p>My first deployment.</p>
+
+      <button>Coming Soon</button>
+    </div>
+  );
+}
+
+export default App;
